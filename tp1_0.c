1e5be4b761ceb5b6adfd4a220969d9dd043e302c
Valalol <valentin.esnault92@gmail.com>
2024-02-08 16:26:00 +0100

    piles qui marchent et commentaires

--- tp1_0.c
+++ tp1_0.c
@@ -4,12 +4,14 @@
 #include "pile.h"
 
 int gml_check(FILE *pFile) {
+    // on crée une pile et une chaine de caractères
     struct Pile pile_balises;
     init_pile(&pile_balises);
 
     char string_array[20];
     int n_string = 0;
 
+    // On crée nos états et nos types de balises
     enum Balise_type {
         ouvrante,
         fermante
@@ -27,29 +29,50 @@ int gml_check(FILE *pFile) {
     cur_etat = avant_balise;
     char c;
 
+    // tant qu'on est pas à la fin du fichier
     while(!feof(pFile)) {
+        // on récupère le caractère suivant
         c = fgetc(pFile);
 
+        // en fonction de l'état actuel
         switch(cur_etat) {
             case avant_balise:
+            // si on trouve un '<' on commence à lire le type de la balise
             if (c == '<') {
                 cur_etat = lecture_type_balise;
             }
             break;
 
+            case lecture_type_balise:
+            // si on trouve un '/' on sait que c'est une balise fermante sinon c'est une balise ouvrante, 
+            // et on passe à la lecture de la balise en elle meme
+            // S'il y a un caractère autre que '/' on utilise ungetc() pour remettre le caractère dans le flux 
+            if (c == '/') {
+                cur_balise_type = fermante;
+                cur_etat = lecture_balise;
+            } 
+            else {
+                ungetc(c, pFile);
+                cur_balise_type = ouvrante;
+                cur_etat = lecture_balise;
+            }
+            break;
+
             case lecture_balise:
+            // on lit les caractères un par un jusqu'à trouver un espace ou un '>'
             if (c == '>' || c == ' ') {
+                // on termine le string par un \0
                 string_array[n_string] = '\0';
                 n_string = 0;
+                // Si la balise était ouvrante, on l'ajoute à la pile
                 if (cur_balise_type == ouvrante) {
-                    // printf("Balise ouvrante : %s\n", string_array);
                     pilePush(&pile_balises, string_array);
                 } else {
-                    // printf("Balise fermante : %s\n", string_array);
-
+                    // sinon, on compare le dernier élément de la pile avec la balise actuelle
                     if (strcmp(string_array, pileTop(&pile_balises))) {
+                        // si les balises sont différentes, on affiche une erreur et on stoppe le programme
                         printf("Erreur trouvée !\nBalise attendue : </%s> Balise trouvée : </%s>\n", pileTop(&pile_balises), string_array);
-                        exit(0);
+                        return 0;
                     }
                     pilePop(&pile_balises);
                 }
@@ -58,21 +81,6 @@ int gml_check(FILE *pFile) {
             else {
                 string_array[n_string] = c;
                 n_string++;
-            }
-            break;
-
-            case lecture_type_balise:
-            if (c == '/') {
-                cur_balise_type = fermante;
-
-                cur_etat = lecture_balise;
-            } 
-            else {
-                cur_balise_type = ouvrante;
-
-                ungetc(c, pFile);
-
-                cur_etat = lecture_balise;
             }
             break;
         }
